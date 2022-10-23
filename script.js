fffd681ffdd5f215a99e7c31e6f3978b3138b185
Khalid Atayee <khalidatayee.76@gmail.com>
2022-10-23 15:39:31 +0430

    color generator about to finish just working on click a input to be copied

---
+++ script.js
@@ -0,0 +1,33 @@
+const formInput =document.getElementById('color-form');
+let inputField = document.getElementById('color');
+formInput.addEventListener('submit',(event)=>{
+    let newColors = [];
+    event.preventDefault();
+    const fromData = new FormData(formInput);
+
+    let inputColor = fromData.get('color');
+    inputColor=inputColor.slice(1);
+    let baseUrl="https://www.thecolorapi.com";
+
+    let mode = fromData.get('mode');
+
+    fetch(baseUrl + '/scheme?hex='+inputColor+'&mode='+mode)
+    .then(Response=>Response.json())
+    .then((data)=>{
+        let dataColors = data.colors;
+        dataColors.map((color)=>{
+            newColors.push(color.hex.value);
+            
+        });
+        setColors(newColors);
+    })
+
+});
+
+function setColors(colors){
+    for(let i=0; i<colors.length; i++){
+        let color = colors[i];
+        document.getElementById('color'+i).style.backgroundColor = color;
+    }
+
+}
