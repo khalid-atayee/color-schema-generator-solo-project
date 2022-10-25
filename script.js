81e185c8c6483bcb9125d2c32ccd331103f7b0a8
Khalid Atayee <khalidatayee.76@gmail.com>
2022-10-25 15:04:01 +0430

    api color generator done

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const formInput =document.getElementById('color-form');
-let inputField = document.getElementById('color');
+let baseUrl="https://www.thecolorapi.com";
+const footer = document.getElementById('footer');
+const main = document.getElementById('main');
+const alert = document.querySelector('.alert');
 
+
+
+window.addEventListener('load', getColorApi())
 formInput.addEventListener('submit',(event)=>{
 
-    let newColors = [];
     event.preventDefault();
+    getColorApi();
+});
+
+// **** this function responsible for getting data from input fields and fetching data from api  ****
+function getColorApi(){
+    let newColors = [];
     const fromData = new FormData(formInput);
-
     let inputColor = fromData.get('color');
-    inputColor=inputColor.slice(1);
-    let baseUrl="https://www.thecolorapi.com";
-
     let mode = fromData.get('mode');
-
+    inputColor=inputColor.slice(1);
+    
     fetch(baseUrl + '/scheme?hex='+inputColor+'&mode='+mode)
     .then(Response=>Response.json())
     .then((data)=>{
@@ -24,26 +32,31 @@ formInput.addEventListener('submit',(event)=>{
         setColors(newColors);
     })
 
-});
+}
 
+
+// *** this function responsible for seeting colors in view
 function setColors(colors){
     for(let i=0; i<colors.length; i++){
         document.getElementById('color'+i).style.backgroundColor = colors[i];
         document.getElementById('color'+i).value=colors[i]
         document.getElementById('code'+i).textContent=colors[i];
+        document.getElementById('code'+i).value=colors[i];
     }
 }
+
+// and this event listener will responsible for comying the color and showing the alert message
 document.addEventListener('click',(e)=>{
 
-    if(e.target.parentElement==document.getElementById('main')|| e.target.parentElement==document.getElementById('footer')){
+    if(e.target.parentElement==main|| e.target.parentElement==footer){
         let x=e.clientX-40;
         let y=e.clientY-60;
-        document.querySelector('.alert').style.top=y+'px';
-        document.querySelector('.alert').style.left=x+'px';
+        alert.style.top=y+'px';
+        alert.style.left=x+'px';
         navigator.clipboard.writeText(e.target.value);
-        document.querySelector('.alert').classList.add('show-message')
+        alert.classList.add('show-message')
         setTimeout(function(){
-            document.querySelector('.alert').classList.remove('show-message')
+            alert.classList.remove('show-message')
         },500)
     }
 
